0650b6016b1bd50b5fbf7b8f9d7b66f1b4bd4e77
tewodros-m <tewodrosmengie@gmail.com>
2025-07-29 17:32:40 +0300

    update timestamp fields

--- backend/models/Session.js
+++ backend/models/Session.js
@@ -1,25 +1,23 @@
 import mongoose from 'mongoose';
 import { SessionStatus } from '../enums/sessionStatus.js';
 
-const sessionSchema = new mongoose.Schema(
-  {
-    user_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    title: { type: String, required: true },
-    tags: [String],
-    json_file_url: String,
-    status: {
-      type: String,
-      enum: Object.values(SessionStatus),
-      default: SessionStatus.DRAFT,
-    },
+const sessionSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  title: { type: String, required: true },
+  tags: [String],
+  json_file_url: String,
+  status: {
+    type: String,
+    enum: Object.values(SessionStatus),
+    default: SessionStatus.DRAFT,
+  },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+});
 
-module.exports = mongoose.model('Session', sessionSchema);
+const Session = mongoose.model('Session', sessionSchema);
+export default Session;
