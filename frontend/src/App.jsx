7974752232dbd88f2cd68d02bca6e71d032a8eb3
tewodros-m <tewodrosmengie@gmail.com>
2025-07-29 21:03:38 +0300

    set up frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='min-h-screen bg-gray-100 text-center p-6'>
+      <h1 className='text-2xl font-bold text-blue-600'>
+        Welcome to your Wellness Session
+      </h1>
+    </div>
+  );
+}
+
+export default App;
