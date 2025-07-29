7122ea7bc66cfc4e20434f7836783db57dbf6749
tewodros-m <tewodrosmengie@gmail.com>
2025-07-29 15:51:44 +0300

    set up the backend and connect to database

---
+++ backend/app.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import authRoutes from './routes/authRoutes.js';
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Root test route
+app.get('/', (req, res) => {
+  res.send('Arvyax backend is running...');
+});
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI || !process.env.JWT_SECRET) {
+  console.error('❌ Missing environment variables!');
+  process.exit(1);
+}
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => console.error('MongoDB connection failed:', err));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal server error' });
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `${req.originalUrl} route not found`,
+  });
+});
