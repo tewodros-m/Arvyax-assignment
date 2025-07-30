fc1e54ee7a097608c665aef74f86db0dbba3add0
tewodros-m <tewodrosmengie@gmail.com>
2025-07-31 00:16:51 +0300

    add start script and configure CORS

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,12 @@ import { connectDB } from './config/db.js';
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
